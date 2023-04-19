3085e14e6553e175a5dee54bf719e98a864f53e7
Pavel Shevchuk <pavelrusk@gmail.com>
2023-04-19 23:51:38 +0300

    update

--- ChangeInt.java
+++ ChangeInt.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class ChangeInt {
     public static void main(String[] args) {
         String a = "44";
         int x = Integer.parseInt(a);
         System.out.println(x);
+        String name = "Rom";
+        System.out.println(name);
+        String name2 = name.toLowerCase();
+        System.out.println(name2);
+
+        Scanner console = new Scanner(System.in);
+        String line = console.nextLine();
+        int age = console.nextInt();
+        System.out.println("Name: " + line);
+        System.out.println("Age: " + age);
+
     }
 }

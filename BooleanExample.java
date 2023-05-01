33f0a3349f1ce355c234c93e350eb2f068551d63
Pavel Shevchuk <pavelrusk@gmail.com>
2023-05-01 23:16:06 +0300

    boolean

---
+++ BooleanExample.java
@@ -0,0 +1,12 @@
+/**
+ * BooleanExample
+ */
+public class BooleanExample {
+
+    public static void main(String[] args) {
+        int age = 70;
+        boolean isSenior = (age > 65);
+        if (isSenior)
+            System.out.println("Пора на пенсию");
+    }
+}

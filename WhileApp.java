0f010772f189f83e590dd30f27512194481c1e4a
Pavel Shevchuk <pavelrusk@gmail.com>
2023-05-06 14:00:19 +0300

    update

--- WhileApp.java
+++ WhileApp.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class WhileApp {
-
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         int max = 0;
@@ -11,6 +11,10 @@ public class WhileApp {
                 max = x;
         }
         System.out.println(max);
+        int m = (int) Math.round(4.1);
+        System.out.println(m);
+        System.out.println(Math.max(Double.NaN, 3));
+        double b = 0.000000001;
+        System.out.println(b);
     }
-
 }

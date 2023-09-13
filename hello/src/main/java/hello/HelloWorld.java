30d665292c6021e51ba0a4819108c47362f18593
Pavel Shevchuk <pavelrusk@gmail.com>
2023-09-14 00:31:49 +0300

    Add First API

---
+++ hello/src/main/java/hello/HelloWorld.java
@@ -0,0 +1,8 @@
+package hello;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Greeting greeter = new Greeting();
+        System.out.println(greeter.sayHello());
+    }
+}

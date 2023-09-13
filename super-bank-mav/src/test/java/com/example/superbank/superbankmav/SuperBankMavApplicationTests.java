dded6b4b7dc7a955032ad38673eaa7454ded80eb
Pavel Shevchuk <pavelrusk@gmail.com>
2023-09-14 00:39:16 +0300

    Add Spring Web-service

---
+++ super-bank-mav/src/test/java/com/example/superbank/superbankmav/SuperBankMavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.superbank.superbankmav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperBankMavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

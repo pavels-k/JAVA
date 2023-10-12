f0f5349efd5d760a422a3478abf13fa3d8624770
Pavel Shevchuk <pavelrusk@gmail.com>
2023-10-12 23:38:23 +0300

    update udemy 1.7

---
+++ spring_course/src/spring_introduction/Test3.java
@@ -0,0 +1,17 @@
+package spring_introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test3 {
+    public static void main(String[] args) {
+//        Pet pet = new Dog();
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Pet pet = context.getBean("myPet", Pet.class);
+
+        Person person = context.getBean("myPerson", Person.class);
+        person.callYourPet();
+
+        Object i = new Object();
+        context.close();
+    }
+}

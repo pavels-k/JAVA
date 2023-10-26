807718aeccf207aa8589832e4226ed95a6db6858
Pavel Shevchuk <pavelrusk@gmail.com>
2023-10-26 23:46:17 +0300

    Add static method

---
+++ untitled/src/StaticMethod.java
@@ -0,0 +1,8 @@
+ public class StaticMethod {
+    public static void main(String[] args) {
+        hello();
+    }
+    public static void hello(){
+        System.out.println("Hello");
+    }
+}
---
+++ untitled/src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        Class a = String.class;
+        System.out.println(a);
+        Class b = "Привет".getClass();
+        System.out.println(b);
+    }
+}

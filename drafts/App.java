1593283c33f3aa80d4cb180c05ed8e5bf5c9d489
Pavel Shevchuk <pavelrusk@gmail.com>
2023-11-07 17:19:28 +0300

    Update

--- drafts/App.java
+++ drafts/App.java
@@ -1,20 +1,18 @@
-
-
-
 public class App {
     public static void main(String[] args) {
         System.out.println(Integer.toBinaryString(25));
     }
 
     // END
-}
+    public static class Pair {
+        int x, y;
 
-
-public static class Pair{
-    int x, y;
-    public void swap(){
-        int xor = x ^ y;
-        x = xor & y;
-        y = xor & x;
+        public void swap() {
+            int xor = x ^ y;
+            x = xor & y;
+            y = xor & x;
+        }
     }
 }
+
+

27fba3a588cb7d4a005ee07a366e905ebdb2310a
Pavel Shevchuk <pavelrusk@gmail.com>
2023-12-12 22:39:05 +0300

    write part of program

--- Hangman/src/main/java/org/example/Main.java
+++ Hangman/src/main/java/org/example/Main.java
@@ -2,9 +2,11 @@ package org.example;
 
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 import static java.lang.System.in;
+
 
 public class Main {
     public static ArrayList<String> getWord() {
@@ -26,17 +28,56 @@ public class Main {
         System.out.println(listWord.get(0));
 
         System.out.println("Введите команду:");
+
+        System.out.println("1 - Начать игру");
+        System.out.println("0 - Выйти из игры");
         Scanner scanner = new Scanner(in);
         int command = scanner.nextInt();
 
         while (command != 0) {
             switch (command) {
-                case (1) -> System.out.println("Введи букву");
+                case (1) -> {
+                    Random rand = new Random();
+                    int randIndex = rand.nextInt(listWord.size());
+                    String word = listWord.get(randIndex);
+
+                    String currentStateString = copies("_", word.length());
+                    StringBuilder currentState = new StringBuilder(currentStateString);
+
+                    System.out.println("Состояние виселицы:");
+                    System.out.println(currentState);
+
+                    // Написать условие работы цикла
+                    while (currentState.toString().contains("_")) {
+                        System.out.println("Введи букву:");
+                        char c = scanner.next().charAt(0);
+                        if (word.contains(String.valueOf(c))) {
+                            int indexToReplace = currentState.indexOf((String.valueOf(c)));
+                            currentState.setCharAt(indexToReplace, c);
+                            System.out.println();
+                        }
+                    }
+                }
+
+
                 case (3) -> System.out.println("Пропустить код");
                 default -> {
                 }
             }
+            System.out.println("Введите команду:");
             command = scanner.nextInt();
         }
+    }
+
+    public static String copies(String s, int n) {
+        StringBuilder sb = new StringBuilder();
+        if (n > 0) {
+            int i = 0;
+            while (i < n) {
+                sb.append(s);
+                i++;
+            }
+        }
+        return sb.toString();
     }
 }

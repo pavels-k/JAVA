25c96a28a4107ba1784c42258865ce234f7bd5a7
Pavel Shevchuk <pavelrusk@gmail.com>
2023-12-14 23:23:34 +0300

    formate code

--- Hangman/src/main/java/org/example/Main.java
+++ Hangman/src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Random;
@@ -12,7 +13,7 @@ public class Main {
         InputStream is = classloader.getResourceAsStream("russian_nouns.txt");
 
         assert is != null;
-        Scanner scanner = new Scanner(is, "UTF-8");
+        Scanner scanner = new Scanner(is, StandardCharsets.UTF_8);
         ArrayList<String> listWords = new ArrayList<>();
         while (scanner.hasNext()) {
             String word = scanner.next();
@@ -59,76 +60,73 @@ public class Main {
         int command = scanner.nextInt();
 
         while (command != 0) {
-            switch (command) {
-                case (1) -> {
-                    Random rand = new Random();
-                    int randIndex = rand.nextInt(listWord.size());
-                    String strartWord = listWord.get(randIndex);
-                    StringBuilder word = new StringBuilder(strartWord);
+            if (command == 1) {
+                Random rand = new Random();
+                int randIndex = rand.nextInt(listWord.size());
+                String startWord = listWord.get(randIndex);
+                StringBuilder word = new StringBuilder(startWord);
 
-                    String currentStateString = copies("_", word.length());
-                    StringBuilder currentState = new StringBuilder(currentStateString);
-                    final int countTry = 7;
-                    StringBuilder stepHangman = new StringBuilder(copies("o", countTry));
-                    int countMistake = 0;
-                    HashSet<String> usedLetters = new HashSet<>();
+                String currentStateString = copies("_", word.length());
+                StringBuilder currentState = new StringBuilder(currentStateString);
+                final int countTry = 7;
+                StringBuilder stepHangman = new StringBuilder(copies("o", countTry));
+                int countMistake = 0;
+                HashSet<String> usedLetters = new HashSet<>();
 
-                    while (currentState.toString().contains("_") & (countMistake < countTry)) {
-                        System.out.println("Загаданное слово:");
-                        System.out.println(currentState);
-                        System.out.println("\nВведите букву:");
-                        char c = 's';
-                        c = Character.toLowerCase(scanner.next().charAt(0)); // Character.toLowerCase(
+                while (currentState.toString().contains("_") & (countMistake < countTry)) {
+                    System.out.println("Загаданное слово:");
+                    System.out.println(currentState);
+                    System.out.println("\nВведите букву:");
+                    char c = Character.toLowerCase(scanner.next().charAt(0));
 
-                        while (!Character.UnicodeBlock.of(c).equals(Character.UnicodeBlock.CYRILLIC)
-                                | usedLetters.contains(String.valueOf(c))) {
-                            if (usedLetters.contains(String.valueOf(c))) {
-                                System.out.println("\nЭта буква, уже использовалась! Введите другую:");
-                            } else {
-                                System.out.println("\nЭто не кириллица! Введите букву кириллицы: ");
-                            }
-
-                            c = Character.toLowerCase(scanner.next().charAt(0)); // Character.toLowerCase(
-                        }
-
-                        String letter = String.valueOf(c);
-                        usedLetters.add(letter);
-                        if (word.toString().contains(String.valueOf(c))) {
-                            System.out.println("БИНГО!\n");
-
-                            int indexToReplace = word.indexOf(letter);
-                            while (indexToReplace >= 0) {
-
-                                currentState.setCharAt(indexToReplace, c);
-                                word.setCharAt(indexToReplace, '_');
-
-                                indexToReplace = word.indexOf(letter, indexToReplace + 1);
-                            }
-
+                    while (!Character.UnicodeBlock.of(c).equals(Character.UnicodeBlock.CYRILLIC)
+                            | usedLetters.contains(String.valueOf(c))) {
+                        if (usedLetters.contains(String.valueOf(c))) {
+                            System.out.println("\nЭта буква, уже использовалась! Введите другую:");
                         } else {
-                            System.out.println("МИМО!\n");
-
-                            stepHangman.setCharAt(countMistake, 'x');
-
-                            countMistake++;
+                            System.out.println("\nЭто не кириллица! Введите букву кириллицы: ");
                         }
-                        System.out.println("Использованные буквы:");
 
-                        System.out.println(usedLetters.toString());
-
-                        System.out.println("\nСостояние виселицы:");
-
-                        System.out.println(stepHangman);
-
-                        System.out.println();
+                        c = Character.toLowerCase(scanner.next().charAt(0)); // Character.toLowerCase(
                     }
-                    if (stepHangman.toString().contains("o")) {
-                        System.out.println("Миссия выполнена!\n");
+
+                    String letter = String.valueOf(c);
+                    usedLetters.add(letter);
+                    if (word.toString().contains(String.valueOf(c))) {
+                        System.out.println("БИНГО!\n");
+
+                        int indexToReplace = word.indexOf(letter);
+                        while (indexToReplace >= 0) {
+
+                            currentState.setCharAt(indexToReplace, c);
+                            word.setCharAt(indexToReplace, '_');
+
+                            indexToReplace = word.indexOf(letter, indexToReplace + 1);
+                        }
+
                     } else {
-                        System.out.println("Потрачено!\n");
-                        System.out.println("Ну а правильное слово: " + strartWord + "\n");
+                        System.out.println("МИМО!\n");
 
+                        stepHangman.setCharAt(countMistake, 'x');
+
+                        countMistake++;
                     }
+                    System.out.println("Использованные буквы:");
+
+                    System.out.println(usedLetters);
+
+                    System.out.println("\nСостояние виселицы:");
+
+                    System.out.println(stepHangman);
+
+                    System.out.println();
+                }
+                if (stepHangman.toString().contains("o")) {
+                    System.out.println("Миссия выполнена!\n");
+                } else {
+                    System.out.println("Потрачено!\n");
+                    System.out.println("Ну а правильное слово: " + startWord + "\n");
+
                 }
             }
             printGame();

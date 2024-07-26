e961a65c272d7f820ae6986ec037258c14236c51
Pavel Shevchuk <pavelrusk@gmail.com>
2024-07-27 00:28:04 +0300

    update

--- drafts/App.java
+++ drafts/App.java
@@ -1,3 +1,5 @@
+import org.apache.commons.lang3.StringUtils;
+
 public class App {
 
     private static boolean isLeapYear(int year) {
@@ -7,9 +9,15 @@ public class App {
         return false;
     }
 
+    public static String convertString(String word) {
+        if (Character.isUpperCase(word.charAt(0))) {
+            return StringUtils.reverse(word);
+        }
+        return word;
+    }
+
     public static void main(String[] args) {
         System.out.println(isLeapYear(2016));
+        System.out.println(isLeapYear(2023));
     }
 }
-
-

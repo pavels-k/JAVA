60613e28a1415a6f3ec86bd9b39b7220d03f408a
Pavel Shevchuk <pavelrusk@gmail.com>
2024-07-27 00:36:25 +0300

    Add udemy folder

---
+++ udemy/first.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class first {
+    public static void main(String[] args) {
+        // Create a new ArrayList of Strings
+        ArrayList al = new ArrayList();
+        al.add(10);
+        al.add("Stt");
+        System.out.println(al.toString());
+    }
+}

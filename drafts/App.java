1cffcc628c22ed813ac1345f7e8aa35f4f0a7531
Pavel Shevchuk <pavelrusk@gmail.com>
2024-07-28 19:21:06 +0300

    update

--- drafts/App.java
+++ drafts/App.java
@@ -10,14 +10,52 @@ public class App {
     }
 
     public static String convertString(String word) {
-        if (Character.isUpperCase(word.charAt(0))) {
-            return StringUtils.reverse(word);
+        if (word.length() == 0) {
+            return word;
+        } else if (Character.isUpperCase(word.charAt(0))) {
+            return word;
         }
-        return word;
+        return StringUtils.reverse(word);
     }
 
+    public static String getNumberExplanation(Integer number) {
+        switch (number) {
+            case 666:
+                return "devil number";
+            case 42:
+                return "answer for everything";
+            case 7:
+                return "prime number";
+            default:
+                return "just number";
+        }
+    }
+
+    public static void printNumbers(Integer number) {
+        while (number > 0) {
+            System.out.println(number);
+            number -= 1;
+        }
+        System.out.println("finished!");
+    }
+
+    public static String joinNumbersFromRange(int start, int finish) {
+        // BEGIN (write your solution here)
+        int i = start;
+        String result = "";
+        while (i <= finish) {
+            result += i;
+            i += 1;
+        }
+        return result;
+        // END
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(isLeapYear(2016));
-        System.out.println(isLeapYear(2023));
+
+        //        printNumbers(4);
+        System.out.println(joinNumbersFromRange(1, 2));
+
     }
 }

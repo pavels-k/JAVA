c1cc4a9d59c32255f7b0887c587397e5eda39653
Pavel Shevchuk <pavelrusk@gmail.com>
2024-08-24 00:15:58 +0300

    commit Lesson20

--- alishev/lessons/src/Lesson20.java
+++ alishev/lessons/src/Lesson20.java
@@ -1,12 +1,19 @@
 public class Lesson20 {
     public static void main(String[] args) {
-        Human human1 = new Human();
-        human1.setAge(40);
-        human1.setName("eqwda");
-
+        Human human1 = new Human("dswad", 2);
+        Human human2 = new Human("Pavel", 22);
+        human1.getAllFields();
         Human.description = "Nice";
+        human2.getAllFields();
 
-        Human human2 = new Human("sdaw");t
+
+        System.out.println();
+
+        System.out.println(Human.getDescription());
+
+        System.out.println((Math.pow(2, 4)));
+        System.out.println((Math.PI));
+
     }
 }
 
@@ -14,19 +21,16 @@ class Human {
     private String name;
     private int age;
 
+    private static int countPeople = 0;
+
     public static String description;
 
-    public Human() {
-        System.out.println("Привет из первого конструктора!");
-    }
 
-    public Human(String name) {
-        this.name = name;
-    }
 
     public Human(String name, int age) {
         this.name = name;
         this.age = age;
+        countPeople++;
     }
 
     public void setName(String name) {
@@ -36,4 +40,20 @@ class Human {
     public void setAge(int age) {
         this.age = age;
     }
+
+    public static String getDescription() {
+        return description;
+    }
+
+    public void getAllFields() {
+        System.out.println(name + "," + age + "," + description);
+    }
+
+    public void printNUmberOfPeople() {
+        System.out.println("Number of people is" + countPeople);
+    }
+
+//    public static void printAllFields() {
+//        System.out.println(name);
+//    }
 }

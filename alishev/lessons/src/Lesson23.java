c207c3216a44071bb8fbfb31f9eb3f6e5815be32
Pavel Shevchuk <pavelrusk@gmail.com>
2024-08-25 23:43:10 +0300

    commit Lesson23

--- alishev/lessons/src/Lesson23.java
+++ alishev/lessons/src/Lesson23.java
@@ -4,5 +4,9 @@ public class Lesson23 {
         sb.append(" my");
         sb.append(" friend");
         System.out.println(sb.toString());
+        System.out.printf("This is a string, %s\n", "NICE");
+        System.out.printf("String %10d\n", 10000);
+        System.out.printf("String %.2f \n", 45.43245);
     }
 }
+// hello world

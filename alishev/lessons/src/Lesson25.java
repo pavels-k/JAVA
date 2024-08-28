fffc759dbfaf7f48f5f724e1748acdf6eb8b8eb9
Pavel Shevchuk <pavelrusk@gmail.com>
2024-08-28 19:59:28 +0300

    commit Lesson25

--- alishev/lessons/src/Lesson25.java
+++ alishev/lessons/src/Lesson25.java
@@ -4,5 +4,7 @@ public class Lesson25 {
     public static void main(String[] args) {
         Animal animal = new Animal();
         animal.sleep();
+        Dog dog = new Dog();
+        dog.bark();
     }
 }

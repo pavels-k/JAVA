ba70832f979a8d0790ac6323e00df776ea2a32d5
Pavel Shevchuk <pavelrusk@gmail.com>
2024-08-31 23:24:01 +0300

    Commit lesson26

--- alishev/lessons/src/Interfaces/Animal.java
+++ alishev/lessons/src/Interfaces/Animal.java
@@ -1,9 +1,19 @@
 package Interfaces;
 
-public class Animal {
+public class Animal implements Info{
     public int id;
 
+    public Animal(int id) {
+        this.id = id;
+    }
+
     public void sleep(){
+
         System.out.println("I am sleeping");
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.println("Id is" + this.id);
     }
 }
--- alishev/lessons/src/Interfaces/Person.java
+++ alishev/lessons/src/Interfaces/Person.java
@@ -1,9 +1,16 @@
 package Interfaces;
 
-public class Person {
+public class Person implements Info{
     public String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
     public void sayHello(){
         System.out.println("Hello");
     }
-
+    public void showInfo(){
+        System.out.println("Id is" + this.id);
+    }
 }

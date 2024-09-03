3a21b8012c33b9dab842207ccd756bc7e332071f
Pavel Shevchuk <pavelrusk@gmail.com>
2024-09-03 23:51:04 +0300

    commit Lesson26

--- alishev/lessons/src/Interfaces/Test.java
+++ alishev/lessons/src/Interfaces/Test.java
@@ -2,9 +2,12 @@ package Interfaces;
 
 public class Test {
     public static void main(String[] args) {
-        Info info1 = new Animal(1);
-        Info info2 = new Person("Bob");
-        info1.showInfo();
-        info2.showInfo();
+        Animal animal1 = new Animal(1);
+        Person person1 = new Person("Bob");
+        outputInfo(animal1);
+        outputInfo(person1);
+    }
+    public static void outputInfo(Info info){
+        info.showInfo();
     }
 }

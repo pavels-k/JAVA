6829d902d7bdd78b682269d26651d25ee84f5c48
Pavel Shevchuk <pavelrusk@gmail.com>
2024-09-04 22:28:04 +0300

    commit lesson28

---
+++ alishev/lesson27/LESSON27/src/Test.java
@@ -0,0 +1,13 @@
+import Forest.Squirell;
+import Forest.Tree;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Tree tree1 = new Tree();
+        Squirell squirell = new Squirell();
+
+        Scanner scanner = new Scanner(System.in);
+    }
+}

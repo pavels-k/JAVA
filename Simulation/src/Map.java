16be1abaa8ad45358987a0dffe6cf65cf6a6a3c2
Pavel Shevchuk <pavelrusk@gmail.com>
2024-09-08 13:26:27 +0300

    update Simulation project

--- Simulation/src/Map.java
+++ Simulation/src/Map.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Map {
     //  Хранить координаты каждого существа
@@ -7,11 +8,30 @@ public class Map {
     final int N = 20; // Длина карты
 
     private static HashMap<String, Creature> mapObjects;
-
+    private static ArrayList<ArrayList<Integer>> arrayList = new ArrayList<>();
 
     public Map(){
         mapObjects = new HashMap<>();
+        for (int i = 0; i < M; i++) {
+            ArrayList<Integer> row = new ArrayList<>();
+            for (int j = 0; j < N; j++) {
+                row.add(0);hjghj
+            }
+            arrayList.add(row);
+        }
     }
+
+
+    // расставить существ
+    public void arrangeСreatures(int countHerbivore, int countPredator){
+        for (int i = 0; i < countHerbivore; i++) {
+            ;
+        }
+        for (int i = 0; i < countPredator; i++) {
+            ;
+        }
+    }
+
     public void addObject(Creature creature){
         mapObjects.put(creature.getId(), creature);
     }
--- Simulation/src/Simulation.java
+++ Simulation/src/Simulation.java
@@ -1,8 +1,18 @@
+import javax.swing.*;
+
+
+
 public class Simulation {
     Map map;
     int countStep;
 
-    public static void main(String[] args) {
+    public void initActions(){
         Map map = new Map();
+        
+    }
+
+    public static void main(String[] args) {
+        Actions action = new Actions();
+        map = action.initActions();
     }
 }

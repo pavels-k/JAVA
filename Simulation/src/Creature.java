14b07f366183f7e458b4efeffe452bb9c566ecc4
Pavel Shevchuk <pavelrusk@gmail.com>
2024-10-20 19:41:10 +0300

    add template makeMove Predator

--- Simulation/src/Creature.java
+++ Simulation/src/Creature.java
@@ -26,6 +26,25 @@ public abstract class Creature extends Entity{
         hp = Math.min(hp + healthIncrement, 100);
     }
 
+    // Находится ли в зоне видимости проверяемая координата для существа
+    public boolean isVisible(int x, int xCurrent, int y, int yCurrent) {
+        return (Math.abs(x - xCurrent) > 4) || (Math.abs(y - yCurrent) > 4);
+    }
+
+    // Определить направление хода на одну клетку
+    public int[] getTowards(int x, int y, int xCurrent, int yCurrent) {
+        if (Math.abs(x - xCurrent) > Math.abs((y - yCurrent))) {
+            return new int[]{x - xCurrent > 0 ? 1 : -1, 0};
+        }
+        return new int[]{0, y - yCurrent > 0 ? 1 : -1};
+    }
+
+    // проверка границы карты
+    public boolean isValid(Map map, int xCurrent, int yCurrent) {
+        return (xCurrent >= 0 && xCurrent < map.M && yCurrent >= 0 && yCurrent < map.N);
+    }
+
+
     // движение
     public abstract void makeMove(Map map);
 }
--- Simulation/src/Predator.java
+++ Simulation/src/Predator.java
@@ -1,27 +1,80 @@
-import java.util.UUID;
+import java.util.*;
 
-public class Predator extends Creature{
+public class Predator extends Creature {
     private String id;
 
-    public Predator(int x, int y){
+    public Predator(int x, int y) {
         super(2, x, y);
         this.id = UUID.randomUUID().toString();
     }
 
 
-    // сделать ход
-    public void makeMove(Map map) {
-        ;
-
-    }
-
     private int attackPower;
+
     // переместиться
     // атаковать травоядное
-    private void attackHerbivore(Herbivore herbivore){
+    private void attackHerbivore(Herbivore herbivore) {
         herbivore.takeDamage(attackPower);
     }
 
-    public void makeMove(Map map, Creature creature) {
+    // движение хищника
+    @Override
+    public void makeMove(Map map) {
+        // Пока не увидит траву
+
+        int xCurrent = this.x;
+        int yCurrent = this.y;
+
+        List<int[]> directions = new ArrayList<>();
+        directions.add(new int[]{1, 0});
+        directions.add(new int[]{-1, 0});
+        directions.add(new int[]{0, 1});
+        directions.add(new int[]{0, -1});
+
+        // максимум на 4 хода видит
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{xCurrent, yCurrent});
+        boolean[][] visited = new boolean[map.M][map.N];
+        visited[xCurrent][yCurrent] = true;
+
+
+        while (!queue.isEmpty()) {
+            int[] coordinates = queue.poll();
+            int x = coordinates[0];
+            int y = coordinates[1];
+
+
+            // Выйди если проверяемая координата выходит за пределы видимости
+            if (isVisible(x, xCurrent, y, yCurrent)) {
+                continue;
+            }
+
+
+            // Если найдено травоядное
+            if (map.getCellValue(x, y).equals("H")) {
+                // понять направление, куда нужно идти
+                int[] direction = getTowards(x, y, xCurrent, yCurrent);
+
+                // изменить координату хищнику
+                map.setCellValue(xCurrent, yCurrent, "O");
+                this.x += direction[0];
+                this.y += direction[1];
+                map.setCellValue(this.x, this.y, "P");
+                this.addHp(20);
+                return;
+
+            }
+
+            // Добавить в очередь элемент
+            for (int[] direction : directions) {
+                int newX = x + direction[0];
+                int newY = y + direction[1];
+                if (isValid(map, newX, newY) && !visited[newX][newY]) {
+                    queue.add(new int[]{newX, newY});
+                    visited[newX][newY] = true;
+                }
+            }
+        }
+        ;
     }
 }
